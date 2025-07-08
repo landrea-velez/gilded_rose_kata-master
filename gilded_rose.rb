98df5d724fc8f5f1e8353eb87cd797dc72312ed8
landrea-velez <landrea.velez@gmail.com>
2025-07-08 17:04:29 -0500

    refactor con cursor

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,49 +1,67 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+    update_item_quality(item)
+    update_sell_in(item)
+    handle_expired_item(item)
   end
+end
+
+private
+
+def update_item_quality(item)
+  case item.name
+  when 'Aged Brie'
+    increase_quality(item)
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    update_backstage_pass_quality(item)
+  when 'Sulfuras, Hand of Ragnaros'
+    # Sulfuras never changes quality
+  else
+    decrease_quality(item, conjured_degradation_factor(item))
+  end
+end
+
+def update_sell_in(item)
+  item.sell_in -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
+end
+
+def handle_expired_item(item)
+  return if item.sell_in >= 0
+  
+  case item.name
+  when 'Aged Brie'
+    increase_quality(item)
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    item.quality = 0
+  when 'Sulfuras, Hand of Ragnaros'
+    # Sulfuras never changes
+  else
+    decrease_quality(item, conjured_degradation_factor(item))
+  end
+end
+
+def update_backstage_pass_quality(item)
+  increase_quality(item)
+  
+  if item.sell_in <= 10
+    increase_quality(item)
+  end
+  
+  if item.sell_in <= 5
+    increase_quality(item)
+  end
+end
+
+def increase_quality(item)
+  item.quality += 1 if item.quality < 50
+end
+
+def decrease_quality(item, factor = 1)
+  item.quality -= factor if item.quality > 0
+end
+
+def conjured_degradation_factor(item)
+  item.name.start_with?('Conjured') ? 2 : 1
 end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
